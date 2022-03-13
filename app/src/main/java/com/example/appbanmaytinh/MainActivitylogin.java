9a6206df8474c931e0875a567707fc0df17a8df7
Phan Cong Long <pcl2k2@gmail.com>
2022-03-13 21:43:00 +0700

    Add RegisterActivity and validate form in RegisterActivity

--- app/src/main/java/com/example/appbanmaytinh/MainActivitylogin.java
+++ app/src/main/java/com/example/appbanmaytinh/MainActivitylogin.java
@@ -1,23 +1,33 @@
 package com.example.appbanmaytinh;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivitylogin extends AppCompatActivity {
-Button login;
+    Button login,btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activitylogin);
-        login=findViewById(R.id.login);
+        login = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.Register);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivitylogin.this,MainActivity.class);
+                Intent intent = new Intent(MainActivitylogin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivitylogin.this , RegisterActivity.class);
                 startActivity(intent);
             }
         });
